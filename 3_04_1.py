a6f24cf3f1596aadd6ce75a47b14106bf8903e26
Rlyi <a1065135230@gmail.com>
2018-03-07 10:47:45 +0800

    add comment

--- 3_04_1.py
+++ 3_04_1.py
@@ -1,19 +1,16 @@
 #2018年3月4日 14:46:16
 #颜色选择框
 import sys
-from PyQt5.QtWidgets import (QWidget, QPushButton, QFrame,
-    QColorDialog, QApplication)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QFrame,
+    QColorDialog)
 from PyQt5.QtGui import QColor
 
-
 class Example(QWidget):
-
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-
         col = QColor(0, 0, 0)
 
         self.btn = QPushButton('Dialog', self)
@@ -21,26 +18,21 @@ class Example(QWidget):
         self.btn.clicked.connect(self.showDialog)
 
         self.frm = QFrame(self)
-        self.frm.setStyleSheet("QWidget { background-color: %s }"
-            % col.name())
+        #setStyleSheet(self, str)
+        self.frm.setStyleSheet('background-color:%s' % col.name())
         self.frm.setGeometry(130, 22, 100, 100)
 
         self.setGeometry(300, 300, 250, 180)
-        self.setWindowTitle('Color dialog')
+        self.setWindowTitle('Color Dialog')
         self.show()
 
-
     def showDialog(self):
-
         col = QColorDialog.getColor()
 
         if col.isValid():
-            self.frm.setStyleSheet("QWidget { background-color: %s }"
-                % col.name())
-
+            self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
 
 if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
+    app = QApplication([])
     ex = Example()
     sys.exit(app.exec_())
